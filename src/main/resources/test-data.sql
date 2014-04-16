a664f589480b22a8bb2d311ef6f589a3103cf2cc
Javix <s.cambour@gmail.com>
2014-04-16 17:34:18 +0200

    Remove contacts table creation and population

--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -5,16 +5,3 @@ insert into posts (title) values ('Ruby on Rails');
 insert into comments (text, post_id) values ('Java EE is too difficult and long to study', 1);
 insert into comments (text, post_id) values ('Ruby is very cool and easy to study', 2);
 insert into comments (text, post_id) values ('Ruby on Rails rocks and a must for Web development', 3);
-
-insert into contact (first_name, last_name, birth_date) values ('Clarence', 'Ho', '1980-07-30');
-insert into contact (first_name, last_name, birth_date) values ('Scott', 'Tiger', '1990-11-02');
-insert into contact (first_name, last_name, birth_date) values ('John', 'Smith', '1964-02-28');
-insert into contact (first_name, last_name, birth_date) values ('Peter', 'Jackson', '1944-1-10');
-insert into contact (first_name, last_name, birth_date) values ('Jacky', 'Chan', '1955-10-31');
-insert into contact (first_name, last_name, birth_date) values ('Susan', 'Boyle', '1970-05-06');
-insert into contact (first_name, last_name, birth_date) values ('Tinner', 'Turner', '1967-04-30');
-insert into contact (first_name, last_name, birth_date) values ('Lotus', 'Notes', '1990-02-28');
-insert into contact (first_name, last_name, birth_date) values ('Henry', 'Dickson', '1997-06-30');
-insert into contact (first_name, last_name, birth_date) values ('Sam', 'Davis', '2001-01-31');
-insert into contact (first_name, last_name, birth_date) values ('Max', 'Beckham', '2002-02-01');
-insert into contact (first_name, last_name, birth_date) values ('Paul', 'Simon', '2002-02-28');
